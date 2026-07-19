9d0675baed2d02bc4a423cb5998c66af8cf319cf
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-07-19 23:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from models import db, Flower
 
 app = Flask(__name__)
+
+# SQLite Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flowers.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize Database
+db.init_app(app)
+
+# Create database tables automatically
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    flowers = Flower.query.all()
+    return render_template("index.html", flowers=flowers)
 
 
 if __name__ == "__main__":
